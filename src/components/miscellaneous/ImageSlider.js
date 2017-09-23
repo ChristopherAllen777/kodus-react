7c088a622970c2e3177883b0a429ed05ed3ed869
Cody Childers <cody_k_childers@homedepot.com>
2017-09-23 17:56:10 -0500

    deactivate buttons when needed

--- src/components/miscellaneous/ImageSlider.js
+++ src/components/miscellaneous/ImageSlider.js
@@ -31,18 +31,28 @@ export default class ImageSlider extends MyComponent {
     render() {
         let image_url = this.props.images[this.state.image_index];
 
+        let leftButtonText = "&#10094;";
+        if (this.state.image_index == 0) {
+            leftButtonText = null;
+        }
+
+        let rightButtonText = "&#10095;";
+        if (this.state.image_index == (this.props.images.length - 1)) {
+            rightButtonText = null;
+        }
+
         return (
             <div style={ { width: '220px', display: 'block', margin: 'auto', width: '100%',  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }} className="w3-content w3-display-container">
                 <img className="mySlides" src={image_url} style={ { width: '100%' } } />
                 <button
                     className="w3-button w3-black w3-display-left"
                     onClick={() => this.handleGoLeft()}>
-                        &#10094;
+                        {leftButtonText}
                 </button>
                 <button style= { { float: 'right' } }
                     className="w3-button w3-black w3-display-right"
                     onClick={() => this.handleGoRight()}>
-                        &#10095;
+                        {rightButtonText}
                 </button>
             </div>
         )

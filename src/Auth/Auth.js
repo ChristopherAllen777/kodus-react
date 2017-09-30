00e59e4be772f9a5a830ade72d3d50d82ec4117b
Samuel Jeon <samueljeon@Samuels-MacBook-Air.local>
2017-09-30 10:04:25 -0500

    log in fixed

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -33,9 +33,9 @@ export default class Auth extends Component {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
-          window.location = '/thankyou';
+          window.location = '/';
       } else if (err) {
-        window.location = '/thankyou';
+        window.location = '/';
         console.log(err);
         alert(`Error: ${err.error}. Check the console for further details.`);
       }
@@ -49,7 +49,7 @@ export default class Auth extends Component {
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
     // navigate to the home route
-    window.location = '/thankyou';
+    window.location = '/';
   }
 
   getAccessToken() {
@@ -76,7 +76,7 @@ export default class Auth extends Component {
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
     // navigate to the home route
-    window.location = '/thankyou';
+    window.location = '/';
   }
 
   isAuthenticated() {
